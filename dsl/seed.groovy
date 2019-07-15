23d1dd11d33e71fbd29b1bea7d23cd73f86f2ae8
Yanan Fu <yfu@redhat.com>
2019-07-15 19:56:59 +0800

    new-imp branch change
    Signed-off-by: Yanan Fu <yfu@redhat.com>

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -1,3 +1,44 @@
-job(JobName) {
-    description("A simple Freestyle Job generated from seed.groovy")
+#!/usr/bin/env groovy
+import groovy.io.FileType
+
+String jobDir = "${WORKSPACE}/vars/jobs"
+
+
+def retrieveJobs (String jobDir) {
+    def list = []
+    def dir = new File(jobDir)
+    dir.eachFileRecurse (FileType.FILES) { file ->
+        list << file.getName()
+    }
+    return list
+}
+
+def buildPipelineJobs(String jobName){
+    String repoUrl = "https://github.com/hereischen/hola-jenkins.git"
+    println "Seeding job ${jobName}."
+    pipelineJob(jobName) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repoUrl)
+                        }
+                        branches('new-imp')
+                        extensions {
+                            cleanBeforeCheckout()
+                        }
+                    }
+                }
+                scriptPath("vars/jobs/${jobName}")
+            }
+        }
+        // build jobs to make their properties effective
+        queue(jobName)
+    }
+}
+
+def jobList = retrieveJobs (jobDir)
+jobList.each {
+    buildPipelineJobs(it)
 }
